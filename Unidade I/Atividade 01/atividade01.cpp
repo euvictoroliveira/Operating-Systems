348af454e7ebda21d724e75df0a02fba8f2d7227
Victor Oliveira <victoroliver249@gmail.com>
2025-12-07 17:31:38 -0300

    add: adiciona comentários em atividade01.cpp

--- Unidade I/Atividade 01/atividade01.cpp
+++ Unidade I/Atividade 01/atividade01.cpp
@@ -5,26 +5,38 @@
 
 using namespace std;
 
+
+
+// Função responsável por criar o processo neto e execuda comando do shell
 void criarNeto(const char* comando) {
-    pid_t pid = fork();
-    if (pid < 0) {
-        cerr << "Erro ao criar processo N." << endl;
+    pid_t pid = fork(); // cria um novo processo neto
+
+    if (pid < 0) { 
+        cerr << "Erro ao criar processo N." << endl; // caso o fork retorne um valor negativo, entra no caso e aponta erro de criação do processo
         exit(1);
-    } else if (pid == 0) {
-        // Processo N
+    } else if (pid == 0) { // Caso o forl retorne 0, isso indica que nó atual da árvore é o processo neto
+        // O código abaixo substitui o processo atual por um camando do shell
+        // "/bin/sh -c comando" executa strings como: "ls", "pwd", "date" etc.
         execl("/bin/sh", "sh", "-c", comando, (char*)nullptr);
+
+        // Mensangem caso a execução do comando falhe
         cerr << "Falha ao executar comando." << endl;
         exit(1);
     }
+
+    // Nota: Se o PID for maior que 0, isso indica que estamos no processo filho ou pai
 }
 
+// Função responsável por criar o processo filho
 void criarFilho(const char* cmd1, const char* cmd2) {
-    pid_t pid = fork();
-    if (pid < 0) {
+    pid_t pid = fork(); // Cria um processo filho(F1 ou F2)
+
+    if (pid < 0) { // Acusa erro de criação de processo caso o pid seja negativo
         cerr << "Erro ao criar processo filho." << endl;
         exit(1);
     } else if (pid == 0) {
-        // Processo F
+        // Indica que estamos no processo filho F1 ou F2    
+        // Etapa de criação de dois processos netos(cada um excecuta um comando)
         criarNeto(cmd1); // N1 ou N3
         criarNeto(cmd2); // N2 ou N4
 
@@ -32,12 +44,15 @@ void criarFilho(const char* cmd1, const char* cmd2) {
         wait(nullptr);
         wait(nullptr);
 
+        // Log do processo filho
         cout << "Sou processo filho. PID: " << getpid()
              << ", PID do pai: " << getppid() << endl;
-        exit(0);
+
+        exit(0); // processo filhos encerram aqui.
     }
 }
 
+// Função main
 int main() {
     // Criando os filhos F1 e F2
     criarFilho("ls", "pwd");        // F1 -> N1(ls) e N2(pwd)
@@ -47,6 +62,7 @@ int main() {
     wait(nullptr);
     wait(nullptr);
 
+    // Mensagem do processo pai
     cout << "Sou processo pai P1. PID: " << getpid() << endl;
     cout << "Encerrando o programa." << endl;
 
